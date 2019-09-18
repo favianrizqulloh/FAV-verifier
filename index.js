ec24d9b7e03b49b198109a8c1131ae7ddea81060
Favian Rizqulloh <48427319+favianrizqulloh@users.noreply.github.com>
2019-09-18 19:03:31 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('FΛV verifier is ready!');
+});
+
+client.on('message', message => {
+  if (message.content === 'agree' && message.channel.id === "574103717897568279") {
+    const messageRole = message.guild.roles.find(role => role.name === "Member")
+    const messageRoleExists = message.guild.roles.exists(role => role.name === "Member")
+    
+    if(message.member.roles.has(messageRole.id)) return;
+    message.member.addRole(messageRole)
+}
+	console.log(message.content);
+});
+
+client.login(process.env.BOT_TOKEN);
