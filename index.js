34c5e77f825ec102b6e295b79f7e3466fb4de259
Favian Rizqulloh <48427319+favianrizqulloh@users.noreply.github.com>
2019-09-19 10:31:22 +0700

    why???

--- index.js
+++ index.js
@@ -15,7 +15,18 @@ client.on('message', message => {
     message.react('✅')
     message.member.addRole(messageRole)
 }
-	console.log(message.content);
 });
 
 client.login(process.env.BOT_TOKEN);
+
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
