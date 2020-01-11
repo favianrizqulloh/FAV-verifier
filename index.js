8d8109353e0d52663e3050e23cee5ed258e588d7
Favian Rizqulloh <48427319+favianrizqulloh@users.noreply.github.com>
2020-01-11 19:16:35 +0700

    Some fixes and others

--- index.js
+++ index.js
@@ -1,55 +1,65 @@
-'use strict'
-require('dotenv').config()
+"use strict";
+require("dotenv").config();
 
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const chalk = require("chalk");
+const moment = require("moment");
+const http = require("http");
+const { BOT_TOKEN, VERIFICATION_CHANNEL, VERIFIED_ROLE } = process.env;
 
-const client = new Discord.Client({disabledEvents:["RESUMED","GUILD_SYNC","GUILD_CREATE","GUILD_DELETE","GUILD_UPDATE","GUILD_MEMBER_ADD","GUILD_MEMBER_REMOVE","GUILD_MEMBER_UPDATE","GUILD_MEMBERS_CHUNK","GUILD_INTEGRATIONS_UPDATE","GUILD_ROLE_CREATE","GUILD_ROLE_DELETE","GUILD_ROLE_UPDATE","GUILD_BAN_ADD","GUILD_BAN_REMOVE","CHANNEL_CREATE","CHANNEL_DELETE","CHANNEL_UPDATE","CHANNEL_PINS_UPDATE","MESSAGE_DELETE","MESSAGE_UPDATE","MESSAGE_DELETE_BULK","MESSAGE_REACTION_ADD","MESSAGE_REACTION_REMOVE","MESSAGE_REACTION_REMOVE_ALL","USER_UPDATE","USER_NOTE_UPDATE","USER_SETTINGS_UPDATE","PRESENCE_UPDATE","VOICE_STATE_UPDATE","TYPING_START","VOICE_SERVER_UPDATE","RELATIONSHIP_ADD","RELATIONSHIP_REMOVE","WEBHOOKS_UPDATE"]});
-client.once('ready', () => {
-	console.log('FΛV verifier is ready!');
+const client = new Discord.Client({
+  disabledEvents: ["TYPING_START", "TYPING_STOP"],
+  disableEveryone: true
 });
 
-client.on('message', message => {
-  if (message.content === 'agree' && message.channel.id === "574103717897568279") {
-	  if (!message.channel.permissionFor(message.guild.me).serialize().SEND_MESSAGES) return console.error("The bot doesn't have the permission to send messages.")
-	  if (!message.channel.permissionFor(message.guild.me).serialize().ADD_REACTIONS) {
-		  console.error("The bot doesn't have the permission to add reactions.")
-		  message.channel.send("The bot doesn't have the permission to add reactions.")
-		  .then(m=>m.delete(20000))
-		  return
-}
-	  if (!message.channel.permissionFor(message.guild.me).serialize().MANAGE_MESSAGES) {
-		  console.error("The bot doesn't have the permission to delete messages.")
-		  message.channel.send("The bot doesn't have the permission to delete messages.")
-		  .then(m=>m.delete(20000))
-		  return
-}
-    const messageRole = message.guild.roles.find(role => role.name === "Member")
-    if (messageRole == null) return.
-    if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
-	    message.channel.send("The bot doesn't have the permission required to assign roles.")
-	    .then(m=>m.delete(20000))
-	    return
+client.once("ready", () => {
+  console.log(chalk.greenBright("[READY]"), `Logged in as ${client.user.tag} (${client.user.id}) at ${moment().format("DD MMMM YYYY, hh:mm:ss")}`);
+});
+
+client.on("message", message => {
+  if (!message.guild) return;
+  if (message.author.bot) return;
+  if (message.content === "agree" && message.channel.id === VERIFICATION_CHANNEL) {
+    if (!message.channel.permissionsFor(message.guild.me).serialize().SEND_MESSAGES) return console.error("The bot doesn't have the permission to send messages.\nRequired permission: SEND_MESSAGES");
+    if (!message.channel.permissionsFor(message.guild.me).serialize().ADD_REACTIONS) {
+      console.error("The bot doesn't have the permission to add reactions.\nRequired permission: `ADD_REACTIONS`");
+      message.channel.send("The bot doesn't have the permission to add reactions.\nRequired permission: `ADD_REACTIONS`")
+        .then(m => m.delete(20000));
+      return;
     }
-    if (message.guild.me.highestRole.comparePositionTo(messageRole)<1) {
-	    message.channel.send("The position of this role is higher than the bot's highest role,it cannot be assigned by the bot.")
-	    .then(m=>m.delete(20000))
-	    return
+    if (!message.channel.permissionsFor(message.guild.me).serialize().MANAGE_MESSAGES) {
+      console.error("The bot doesn't have the permission to delete messages.\nRequired permission: `MANAGE_MESSAGES`");
+      message.channel.send("The bot doesn't have the permission to delete messages.\nRequired permission: `MANAGE_MESSAGES`")
+        .then(m => m.delete(20000));
+      return;
+    }
+    const messageRole = message.guild.roles.find(role => role.name === VERIFIED_ROLE);
+    if (messageRole == null) return;
+    if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+      message.channel.send("The bot doesn't have the permission required to assign roles.\nRequired permission: `MANAGE_ROLES`")
+        .then(m => m.delete(20000));
+      return;
+    }
+    if (message.guild.me.highestRole.comparePositionTo(messageRole) < 1) {
+      message.channel.send("The position of this role is higher than the bot's highest role, it cannot be assigned by the bot.")
+        .then(m => m.delete(20000));
+      return;
     }
     if (messageRole.managed == true) {
-	    message.channel.send("This is a auto managed role,it cannot be assigned.")
-	    .then(m=>m.delete(20000))
-	    return
+      message.channel.send("This is an auto managed role, it cannot be assigned.")
+        .then(m => m.delete(20000));
+      return;
     }
-    if(message.member.roles.has(messageRole.id)) return;
-    message.react('✅')
+    if (message.member.roles.has(messageRole.id)) return;
+    message.react("✅");
     message.member.addRole(messageRole)
-	  .then(()=>message.delete(5000))
-	  .catch(error => {
-	    console.error(error.stack)
-    message.channel.send(error.stack)
-	    .then(m=>m.delete(20000))
-    })
-}
+      .then(() => message.delete(5000))
+      .catch(error => {
+      console.error(error.stack);
+      message.channel.send(error.stack)
+        .then(m => m.delete(20000));
+    });
+  }
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(BOT_TOKEN);
