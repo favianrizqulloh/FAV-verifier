848d9cd59eb5746b4549b777aa3efed37e114519
asdfugil <42699250+asdfugil@users.noreply.github.com>
2021-10-02 03:56:33 +0000

    base d.js v13 support

--- index.js
+++ index.js
@@ -5,58 +5,69 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const chalk = require("chalk");
 const moment = require("moment");
-const { BOT_TOKEN, VERIFICATION_CHANNEL, VERIFIED_ROLE } = process.env;
+const { BOT_TOKEN, VERIFICATION_CHANNEL, VERIFIED_ROLE, VERIFICATION_MESSAGE, DEBUG } = process.env;
+const ERROR_MESSAGE_TIEMOUT = parseInt(process.env.ERROR_MESSAGE_TIEMOUT);
+const SUCCESS_MESSAGE_TIMEOUT = parseInt(process.env.SUCCESS_MESSAGE_TIMEOUT);
 
 const client = new Discord.Client({
-  disableEveryone: true
+  disableEveryone: true,
+  intents: 513,
+  allowedMentions: { parse : ['users'] },
 });
 
 client.once("ready", () => {
   console.log(chalk.greenBright("[READY]"), `Logged in as ${client.user.tag} (${client.user.id}) at ${moment().format("DD MMMM YYYY, hh:mm:ss")}`);
 });
 
-client.on("message", message => {
+if (DEBUG) client.on('debug', console.log)
+client.on("messageCreate", message => {
+  if (DEBUG) console.log(`[Stage 0] ${message.author.tag} (${message.author.id}) in ${message.channel.name} (${message.channel.id}) of ${message.guild.name} (${message.guild.id}):\n\n${message.content}`)
   if (!message.guild) return;
   if (message.author.bot) return;
-  if (message.content === "agree" && message.channel.id === VERIFICATION_CHANNEL) {
+  if (message.content === VERIFICATION_MESSAGE && message.channel.id === VERIFICATION_CHANNEL) {
+  if (DEBUG) console.log(`[Stage 1] Verification attempt by ${message.author.tag} (${message.author.id})`)
     if (!message.channel.permissionsFor(message.guild.me).serialize().SEND_MESSAGES) return console.error("The bot doesn't have the permission to send messages.\nRequired permission: SEND_MESSAGES");
     if (!message.channel.permissionsFor(message.guild.me).serialize().ADD_REACTIONS) {
       console.error("The bot doesn't have the permission to add reactions.\nRequired permission: `ADD_REACTIONS`");
       message.channel.send("The bot doesn't have the permission to add reactions.\nRequired permission: `ADD_REACTIONS`")
-        .then(m => m.delete({timeout: 20000}));
+        .then(m => setTimeout(() => m.delete(), ERROR_MESSAGE_TIMEOUT));
       return;
     }
     if (!message.channel.permissionsFor(message.guild.me).serialize().MANAGE_MESSAGES) {
       console.error("The bot doesn't have the permission to delete messages.\nRequired permission: `MANAGE_MESSAGES`");
       message.channel.send("The bot doesn't have the permission to delete messages.\nRequired permission: `MANAGE_MESSAGES`")
-        .then(m => m.delete({timeout: 20000}));
+        .then(m => setTimeout(() => m.delete(), ERROR_MESSAGE_TIMEOUT));
       return;
     }
-    const messageRole = message.guild.roles.cache.find(role => role.name === VERIFIED_ROLE);
-    if (messageRole == null) return;
-    if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+    const messageRole = message.guild.roles.cache.get(VERIFIED_ROLE) ||
+	message.guild.roles.cache.find(role => role.name === VERIFIED_ROLE);
+    if (messageRole == null) return console.error('Role ${VERIFIED_ROLE} not found');
+    if (!message.guild.me.permissions.has("MANAGE_ROLES")) {
       message.channel.send("The bot doesn't have the permission required to assign roles.\nRequired permission: `MANAGE_ROLES`")
-        .then(m => m.delete({timeout: 20000}));
+        .then(m => setTimeout(() => m.delete(), ERROR_MESSAGE_TIMEOUT));
       return;
     }
+    if (DEBUG) console.log(`[Stage 2.5] Can assign roles`)
     if (message.guild.me.roles.highest.comparePositionTo(messageRole) < 1) {
       message.channel.send("The position of this role is higher than the bot's highest role, it cannot be assigned by the bot.")
-        .then(m => m.delete({timeout: 20000}));
+        .then(m => setTimeout(() => m.delete(), ERROR_MESSAGE_TIMEOUT));
       return;
     }
+    if (DEBUG) console.log(`[Stage 2.5] Role lower`)
     if (messageRole.managed == true) {
       message.channel.send("This is an auto managed role, it cannot be assigned.")
-        .then(m => m.delete({timeout: 20000}));
+        .then(m => setTimeout(() => m.delete(), ERROR_MESSAGE_TIMEOUT));
       return;
     }
+    if (DEBUG) console.log(`[Stage 3] Permission pass`)
     if (message.member.roles.cache.has(messageRole.id)) return;
     message.react("✅");
     message.member.roles.add(messageRole)
-      .then(() => message.delete({ timeout:5000 }))
+      .then(() => setTimeout(() => message.delete() ,SUCCESS_MESSAGE_TIMEOUT))
       .catch(error => {
-      console.error(error.stack);
+      console.error(error);
       message.channel.send(error.stack)
-        .then(m => m.delete({timeout: 20000}));
+        .then(m => setTimeout(() => m.delete(), ERROR_MESSAGE_TIMEOUT));
     });
   }
 });
