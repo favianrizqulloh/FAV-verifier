480a962375b8f7c18af1ac581e0fb68f4b47a965
Favian Rizqulloh <48427319+favianrizqulloh@users.noreply.github.com>
2021-12-30 19:37:46 +0700

    made an oopsie there

--- easy-start.bat
+++ easy-start.bat
@@ -5,8 +5,8 @@ echo 1. Install (run this once)
 echo 2. Start the bot
 echo Select one: 
 set /p selection=
-if %selection% == 1 goto start
-if %selection% == 2 goto install
+if %selection% == 1 goto install
+if %selection% == 2 goto start
 :install
 echo Installing bot...
 npm i
