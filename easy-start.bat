e7b3a0af1a884cf8a076b8875b5a92de5289adff
Favian Rizqulloh <48427319+favianrizqulloh@users.noreply.github.com>
2021-12-30 19:03:23 +0700

    some detailed instructions & fix the start command

---
+++ easy-start.bat
@@ -0,0 +1,15 @@
+@echo off
+color 0a
+echo Easy Start - fav Verifier Discord bot script
+echo 1. Install (run this once)
+echo 2. Start the bot
+echo Select one: 
+set /p selection=
+if %selection% == 1 goto start
+if %selection% == 2 goto install
+:install
+echo Installing bot...
+npm i
+:start
+echo Starting bot...
+start powershell -noexit -command "node index.js"
